ae462b5cf01dd1c5a71e50bf4a903dc003b399ad
Carlos Hecker <chvodev@gmail.com>
2023-03-14 22:12:08 -0300

    Agregue el enunciado del ejercicio 3, resolvi y testie

--- js/3-ejercicio.js
+++ js/3-ejercicio.js
@@ -1,1 +1,12 @@
+// Realiza un script que pida cadenas de texto  hasta que se pulse “cancelar”. Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión -. Nota: usar confirm()
 
+let resultado = "";
+do {
+  const cadenaTexto = prompt("Ingrese una cadena de texto");
+  if (resultado === "") {
+    resultado = `${resultado} ${cadenaTexto}`;
+  } else {
+    resultado = `${resultado} - ${cadenaTexto}`;
+  }
+} while (confirm("Desea continuar ingresando cadena de texto?"));
+document.write(resultado);
