a3a8ef9779aa8b7bdf34bfebd77fc6973fde7dcc
Carlos Hecker <chvodev@gmail.com>
2023-03-14 22:50:05 -0300

    Agregue el ejercicio 7, resolvi y testie

--- js/7-ejercicio.js
+++ js/7-ejercicio.js
@@ -1,1 +1,26 @@
+/* Haz un script que escriba una pirámide inversa de los números del 1 al número que indique el usuario (no mayor de 50) de la siguiente forma : (suponiendo que indica 30).
+303030303030303030303030303030303030303030303030303030303030
+2929292929292929292929292929292929292929292929292929292929
+28282828282828282828282828282828282828282828282828282828
+…..
+333
+22
+1 
+*/
 
+let numrep = prompt("Introduce numero de repeticiones");
+if (numrep == numrep) {
+    if (numrep > 0 && numrep <= 50) {
+      let rep, i;
+      document.write("<h1>| PIRAMIDE INVERSA |</h1>");
+        for (i = numrep; i >= 1; i--) {
+            for (rep = i; rep >= 1; rep--) {
+                document.write(i);
+            }
+            document.write("<br>");
+        }
+    } 
+    else {
+        alert("El número introducido no es válido");
+    }
+} 
