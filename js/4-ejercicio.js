0cd6e2c4b82b23d74a4d59a08d45080df7bf61ec
Carlos Hecker <chvodev@gmail.com>
2023-03-14 22:19:08 -0300

    Agregue el enunciado del ejercicio 4, resolvi y testie

--- js/4-ejercicio.js
+++ js/4-ejercicio.js
@@ -1,1 +1,14 @@
+// Realiza un script que pida números hasta que se pulse “cancelar”. Si no es un número deberá indicarse con un «alert» y seguir pidiendo números. Al salir con “cancelar” deberá indicarse la suma total de los números introducidos. 
 
+  let pedidoNumero = true;
+  let suma = 0;
+  do {
+    numero = parseInt(prompt("Ingrese un número a sumar"));
+    if (isNaN(numero)) {
+      alert(`${numero} no es un número`);
+    } else {
+      suma += numero;
+      pedidoNumero = confirm("¿Quieres continuar?");
+    }
+  } while (pedidoNumero == true);
+document.write(`La suma de los número introducidos es: ${suma}`);
